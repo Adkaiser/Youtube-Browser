05dc19ea0975b249abd2a13ab21e0c420261790f
Adkaiser <alexdkaiser@gmail.com>
2016-12-06 17:54:13 -0800

    Fetch full list of videos

--- youtube_browser.py
+++ youtube_browser.py
@@ -2,6 +2,7 @@
 import httplib2
 import os
 import sys
+from operator import itemgetter
 from apiclient.discovery import build
 from apiclient.errors import HttpError
 from oauth2client.client import flow_from_clientsecrets
@@ -40,20 +41,57 @@ def saveChannelList(id):
 
 	
 #Get first page, return list of channel ids, and recurse through additional pages
+#@params:
+#youtube: apiclient - youtube api object
+#channel_id: string - id of channel to fetch subscriptions for
+#sub_list: array - list of subscriptions fetched by prior function calls
+#page: string - token of page to fetch on this call
+#@retuns:
+#array - list of subscriptions
 def fetchSubscriptionPage(youtube, channel_id, sub_list, page=None):
 	subscription_response = youtube.subscriptions().list(
 		part='snippet',
 		channelId = channel_id,
-		maxResults = 2,
+		maxResults = 50,
 		pageToken = page
 	).execute()
 	for sub in subscription_response["items"]:
-		sub_list.append({"channel_id":sub["snippet"]["resourceId"]["channelId"]})
+		sub_list.append({"channel_id":sub["snippet"]["resourceId"]["channelId"], "name":sub["snippet"]["title"]})
 		
 	#Recurse to the next token if there is one.
 	if "nextPageToken" in subscription_response:
 		sub_list = fetchSubscriptionPage(youtube, channel_id, sub_list, subscription_response["nextPageToken"])
 	return sub_list
+
+#Get videos on specified channel
+def getVidsFromChannel(youtube, channel):
+	#Find the channel's upload list
+	channel_response = youtube.channels().list(
+		part='contentDetails',
+		id = channel["channel_id"]
+	).execute()
+	uploads = channel_response["items"][0]["contentDetails"]["relatedPlaylists"]["uploads"]
+	
+	#Get the videos on the upload list
+	channel_vids = getVidsFromUploads(youtube, uploads, [], channel["name"])
+	return channel_vids
+	
+#Get videos from a specified playlist
+def getVidsFromUploads(youtube, upload_list, vid_list, channel_name, page=None):
+	#Retrieve the playlist
+	playlist_response = youtube.playlistItems().list(
+		part='snippet',
+		playlistId=upload_list,
+		maxResults = 50,
+		pageToken = page
+	).execute()
+	for vid in playlist_response["items"]:
+		vid_list.append({"title":vid["snippet"]["title"], "link":vid["snippet"]["resourceId"]["videoId"], "time":vid["snippet"]["publishedAt"], "channel":channel_name})
+		
+	#Recurse to the next token if there is one.
+	if "nextPageToken" in playlist_response:
+		vid_list = getVidsFromUploads(youtube, upload_list, vid_list, channel_name, playlist_response["nextPageToken"])
+	return vid_list
 	
 	
 if __name__ == "__main__":
@@ -67,8 +105,13 @@ if __name__ == "__main__":
 	youtube = get_authenticated_service(args)
 	
 	try:
-		channel_title = fetchSubscriptionPage(youtube, newname, [])
+		channels = fetchSubscriptionPage(youtube, newname, [])
+		video_list = []
+		for channel in channels:
+			video_list = video_list + getVidsFromChannel(youtube, channel)
 	except HttpError, e:
 		print "Error %d: %s\n" % (e.resp.status, e.content)
 	else:
-		print channel_title
+		#Sort by time stamp
+		sorted(video_list, key=itemgetter("time"))
+		print video_list
