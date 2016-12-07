b5b2797871c258cfbcb45a88a177debddc895220
Adkaiser <alexdkaiser@gmail.com>
2016-12-07 00:45:06 -0800

    Output results
    Print results to html file
    
    Recall last used channel

--- youtube_browser.py
+++ youtube_browser.py
@@ -2,6 +2,10 @@
 import httplib2
 import os
 import sys
+import time
+import datetime
+import csv
+import codecs
 from operator import itemgetter
 from apiclient.discovery import build
 from apiclient.errors import HttpError
@@ -19,6 +23,8 @@ YOUTUBE_API_VERSION = "v3"
 
 MISSING_CLIENT_SECRET_MESSAGE = "Secret file not found"
 
+CHANNEL_LIST_FILE_NAME = "channel_list.csv"
+
 #Authenticate with youtube and get object to perform actions through
 def get_authenticated_service(args):
 	
@@ -34,11 +40,16 @@ def get_authenticated_service(args):
 		
 	return build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, http=credentials.authorize(httplib2.Http()))
 	
-def saveChannelList(id):
-	f = open("channel_list.csv", "w")
+def saveChannelList(channel_file, id):
+	f = open(channel_file, "w")
 	f.write(id)
 	f.close()
 
+def loadLastChannel(channel_file):
+	with open(channel_file, 'rb') as file:
+		channel_reader = csv.reader(file)
+		return channel_reader.next()[0]
+	
 	
 #Get first page, return list of channel ids, and recurse through additional pages
 #@params:
@@ -64,6 +75,11 @@ def fetchSubscriptionPage(youtube, channel_id, sub_list, page=None):
 	return sub_list
 
 #Get videos on specified channel
+#@params:
+#youtube: apiclient - youtube api object
+#channel: string - id of channel to fetch videos for
+#@retuns:
+#array - list of videos on the channel
 def getVidsFromChannel(youtube, channel):
 	#Find the channel's upload list
 	channel_response = youtube.channels().list(
@@ -73,15 +89,23 @@ def getVidsFromChannel(youtube, channel):
 	uploads = channel_response["items"][0]["contentDetails"]["relatedPlaylists"]["uploads"]
 	
 	#Get the videos on the upload list
-	channel_vids = getVidsFromUploads(youtube, uploads, [], channel["name"])
+	channel_vids = getVidsFromPlaylist(youtube, uploads, [], channel["name"])
 	return channel_vids
 	
 #Get videos from a specified playlist
-def getVidsFromUploads(youtube, upload_list, vid_list, channel_name, page=None):
+#@params:
+#youtube: apiclient - youtube api object
+#playlist: string - id of playlist to fetch vids from
+#vid_list: array - list of videos fetched by prior function calls
+#channel_name: name of channel video is on
+#page: string - token of page to fetch on this call
+#@retuns:
+#array - list of videos in playlist
+def getVidsFromPlaylist(youtube, playlist, vid_list, channel_name, page=None):
 	#Retrieve the playlist
 	playlist_response = youtube.playlistItems().list(
 		part='snippet',
-		playlistId=upload_list,
+		playlistId=playlist,
 		maxResults = 50,
 		pageToken = page
 	).execute()
@@ -90,17 +114,44 @@ def getVidsFromUploads(youtube, upload_list, vid_list, channel_name, page=None):
 		
 	#Recurse to the next token if there is one.
 	if "nextPageToken" in playlist_response:
-		vid_list = getVidsFromUploads(youtube, upload_list, vid_list, channel_name, playlist_response["nextPageToken"])
+		vid_list = getVidsFromPlaylist(youtube, playlist, vid_list, channel_name, playlist_response["nextPageToken"])
 	return vid_list
 	
+#Write list of videos to HTML file
+#@params:
+#vid_list: List of videos
+#@returns:
+#null
+def writeHTML(vid_list):
+	curr_time = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H-%M-%S')
+	file = codecs.open("Subsciption List %s.html" % (curr_time), 'w', 'utf-8')
+	file.write("""<html>
+		<body>
+		""")
+		
+	for video in vid_list:
+		file.write(u"""<a href="https://www.youtube.com/watch?v=%s" target="_blank">%s - %s</a> %s<br/>""" % (video["link"], video["channel"], video["title"], video["time"]))
+		
+	file.write("""
+		</body>
+		</html>
+		""")
+	file.close
+
 	
 if __name__ == "__main__":
 	print "Enter channel ID:"
 	newname = raw_input()
+	
+	#Check for shortcut to last channel entered
+	if newname == "":
+		newname = loadLastChannel(CHANNEL_LIST_FILE_NAME)
+	else:
+		saveChannelList(CHANNEL_LIST_FILE_NAME, newname)
+	
 	argparser.add_argument("--channel-id", help="ID of the channel to subscribe to.",
 		default=newname)
 	args = argparser.parse_args()
-	saveChannelList(newname)
 	#Make API object
 	youtube = get_authenticated_service(args)
 	
@@ -113,5 +164,6 @@ if __name__ == "__main__":
 		print "Error %d: %s\n" % (e.resp.status, e.content)
 	else:
 		#Sort by time stamp
-		sorted(video_list, key=itemgetter("time"))
-		print video_list
+		sorted_list = sorted(video_list, key=itemgetter("time"), reverse=True)
+		#Write list to HTML file
+		writeHTML(sorted_list)
